4ba542e7f97c153ad4ad189c96202e18bce159e5
anurodhacharya <anurodh.acharya10@gmail.com>
2024-08-20 16:08:51 -0700

    Initialize

---
+++ backend/src/main/java/com/aurickcode/File/Sharing/App/FileSharingAppApplication.java
@@ -0,0 +1,13 @@
+package com.aurickcode.File.Sharing.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileSharingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileSharingAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/aurickcode/File/Sharing/App/FileSharingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aurickcode.File.Sharing.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileSharingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
